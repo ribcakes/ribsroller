427577834f5708bcc45ab69348e2c24006a9ef74
thedudestambaugh <thedudestambaugh@gmail.com>
2010-07-14 17:47:37 +0000

    fully commented

--- branches/ver2.0/DiceRoller/src/com/ribcakes/android/projects/dnd1/RotatingText.java
+++ branches/ver2.0/DiceRoller/src/com/ribcakes/android/projects/dnd1/RotatingText.java
@@ -1,10 +1,35 @@
 package com.ribcakes.android.projects.dnd1;
 
+/**
+ * 
+ * @author Brian Stambaugh
+ *
+ * Copyright 2010 Brian Stambaugh
+ * This application is distributed under the terms of the Artistic License 2.0.
+ * 
+ *  This file is part of Rib's Roller.
+ *
+ *   Rib's Roller is free software: you can redistribute it and/or 
+ *   modify it under the terms of the Artistic License 2.0.
+ *
+ *   Rib's Roller is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+ *
+ *  
+ */
+
 import android.content.Context;
 import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+/**
+ * Class used to display Text Fields.  
+ * The view automatically scrolls a line
+ * of text if it is too long for the view, 
+ * and keeps it perpetually scrolling
+ */
 public class RotatingText extends TextView 
 {
 
@@ -26,20 +51,25 @@ public class RotatingText extends TextView
 	}
 
 
-	protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+	//methods prevent the object from losing focus
+	
+	protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect)
+	{
 	    if(focused)
 	        super.onFocusChanged(focused, direction, previouslyFocusedRect);
 	}
 
 	@Override
-	public void onWindowFocusChanged(boolean focused) {
+	public void onWindowFocusChanged(boolean focused) 
+	{
 	    if(focused)
 	        super.onWindowFocusChanged(focused);
 	}
 
 
 	@Override
-	public boolean isFocused() {
+	public boolean isFocused() 
+	{
 	    return true;
 	}
 
